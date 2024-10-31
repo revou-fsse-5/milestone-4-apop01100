75948ebaea8ccd22f9572911aadd9cacfb6e1c08
apop01100 <mthoriqafifr01@gmail.com>
2024-10-31 23:26:40 +0700

    fix configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,24 @@
 from flask import Flask, redirect
-from app.config import DevelopmentConfig
+from app.config import DevelopmentConfig, ProductionConfig
 from flask_jwt_extended import JWTManager
 from flasgger import Swagger
 from app.models.users_model import User
 from app.models.transactions_model import Transaction
 from app.models.accounts_model import Account
 from app.connections.db import Base, engine
+import os
 
 jwt = JWTManager()
 
-def create_app(test_config=None):
+def create_app(test_config=None, production_config=os.getenv("PRODUCTION_CONFIG")):
     app = Flask(__name__)
     
     Base.metadata.create_all(engine)
     
     if test_config is not None:
         app.config.from_object(test_config)
+    if production_config is not None:
+        app.config.from_object(ProductionConfig)
     else:
         app.config.from_object(DevelopmentConfig)
         
